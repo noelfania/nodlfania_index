b5ebd5223f26037da302cf0c63c67915fdecde70
=noelfania <noelfania@naver.com>
2021-02-11 16:21:41 +0900

    automation layout

---
+++ ifr1.js
@@ -0,0 +1,63 @@
+import {Layout} from "./jsController/layout.js";
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const mainArea = document.getElementById('main');
+
+
+// 1
+
+var test1 = new Layout(
+// item area
+`1.itemitemitemitemitemitemitemitemitemitemitemitemitemitemi
+2.temitemitemitemitemitemitemitemitemitemitemitem
+3.itemitemitemitemitemitemitemitemitemitemitemitemitemitemitem`
+,
+// caption area 
+`1.figure : caption_caption_caption_caption_caption_caption_caption_caption_caption
+_caption_caption_caption_caption`
+)
+    var testhtml1 = test1.getfigureTag();
+
+    mainArea.appendChild (testhtml1);
+
+
+
+
+// 2
+var test2 = new Layout(
+// item area
+`1.itemitemitemitemitemitemitemitemitemitemitemitemitemitemi
+2.temitemitemitemitemitemitemitemitemitemitemitem
+3.itemitemitemitemitemitemitemitemitemitemitemitemitemitemitem`
+,
+// caption area 
+`2.figure : caption_caption_caption_caption_caption_caption_caption_caption_caption
+_caption_caption_caption_caption`
+)
+        var testhtml2 = test2.getfigureTag();
+    
+        mainArea.appendChild (testhtml2);
+    
+
+// 3
+var test3 = new Layout(
+// item area
+`1.itemitemitemitemitemitemitemitemitemitemitemitemitemitemi
+2.temitemitemitemitemitemitemitemitemitemitemitem
+3.itemitemitemitemitemitemitemitemitemitemitemitemitemitemitem`
+,
+// caption area 
+`3.figure : caption_caption_caption_caption_caption_caption_caption_caption_caption
+_caption_caption_caption_caption`
+)
+var testhtml3 = test3.getfigureTag();
+
+mainArea.appendChild (testhtml3);
+
+
+
+
+
+
+
+});
--- jsController/layout.js
+++ jsController/layout.js
@@ -1,6 +1,27 @@
 
-function a(){
 
+class Layout{
+    constructor(item, figure){
+        this.item = item;
+        this.figure= figure;
+    }
+    
+    getfigureTag(){
+        var figureTag = document.createElement('figure');
+            figureTag.setAttribute('style', 'border : thin solid black; overflow-wrap: break-word; max-width:70%;');
+        var itemTag = document.createElement('pre');
+            itemTag.innerText = this.item;
+            itemTag.setAttribute('style', 'border-bottom : thin solid black; white-space: break-spaces;margin: 0px;padding: 2%;');
+        var figcaptionTag = document.createElement('pre');
+            figcaptionTag.innerText = this.figure;
+            figcaptionTag.setAttribute('style', 'border-bottom : thin solid black; white-space: break-spaces;margin: 0px;padding: 2%;');
+          
+            figureTag.appendChild(itemTag);
+            figureTag.appendChild(figcaptionTag);
+        
+        return figureTag;
+    }
 }
 
-export { a }
+
+export { Layout }
