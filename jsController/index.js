98541bf261d00358741c075cf4fd8cbe8516eb61
=noelfania <noelfania@naver.com>
2021-02-11 15:20:14 +0900

    modify responsive buttons

--- jsController/index.js
+++ jsController/index.js
@@ -7,6 +7,8 @@ const Index = {
     init(){
         log('DOMContentLoaded :: ', 'app.js invoked');
         //window.Index = Index;
+
+        // link nav-btn with onclick
         var arrNavBtn = document.getElementsByClassName('nav-btn')
         Array.from(arrNavBtn).forEach((navBtn,i)=>{
             navBtn.addEventListener('click',function (event){
@@ -16,12 +18,35 @@ const Index = {
             })
         })
 
-        Index.openPage("ifr1.html", document.getElementsByClassName('nav-btn')[0]);
+        //link title with onclick
+        var arrTitle = document.getElementsByClassName('title');
+        Array.from(arrTitle).forEach((title,i)=>{
+            title.addEventListener('click',function (event){
+                return Index.clickTitleForMobile.call(null, event);
+            })
+        })
+
+        Index.openPage(`${NavBtnUrl[0]}.html`, document.getElementsByClassName('nav-btn')[0]);
     },
+    clickTitleForMobile(event){
+        //initiate all class
+        var allNavBtn = document.getElementsByClassName("nav-btn");
+        Array.from(allNavBtn).forEach((v)=>{
+            v.classList.remove("title_clicked_and_nav-btn_visible")
+        })
+
+        var invisible = event.target.parentNode.getElementsByClassName("nav-btn");
+        Array.from(invisible).forEach((v)=>{
+            v.classList.toggle("title_clicked_and_nav-btn_visible")
+        })
+    }
+    ,
     openPage(url, btnArea, event){
         event ?  event.preventDefault() : null ;
+
         Array.prototype.forEach.call(document.getElementsByClassName('nav-btn'),(v)=>{
             v.classList.remove('nav-btn-clicked');
+            v.classList.remove("title_clicked_and_nav-btn_visible")
             v.parentNode.getElementsByClassName('title')[0].classList.remove('nav-btn-clickedForMobile');
         });
         btnArea.classList.add('nav-btn-clicked');
@@ -31,6 +56,8 @@ const Index = {
         var iframe = document.createElement('iframe');
         iframe.setAttribute('src',url);
         mainContent.appendChild(iframe);
+
+
     }
 };
 
